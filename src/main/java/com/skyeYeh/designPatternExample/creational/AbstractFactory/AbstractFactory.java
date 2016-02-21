0339f9674c4a75321c9e5989f7f616cf69a04c28
SkyeYeh <presto408@gmail.com>
2016-02-21 23:35:34 +0800

    1 duplicated blocks of code must be removed.

--- src/main/java/com/skyeYeh/designPatternExample/creational/AbstractFactory/AbstractFactory.java
+++ src/main/java/com/skyeYeh/designPatternExample/creational/AbstractFactory/AbstractFactory.java
@@ -11,12 +11,20 @@ public interface AbstractFactory {
      * @param type 產品種類
      * @return 產品A
      */
-    public ProductA CreateProductA(String type);
+    default ProductA CreateProductA(String type) {
+        ProductA product = null;
+        if ("productAImpl1".equals(type)) {
+            product = new ProductAImpl1();
+        } else if ("productAImpl2".equals(type)) {
+            product = new ProductAImpl2();
+        }
+        return product;
+    }
 
     /**
      * 抽象工廠建立產品B。
      *
      * @return 產品B
      */
-    public ProductB CreateProductB();
+    ProductB CreateProductB();
 }
--- src/main/java/com/skyeYeh/designPatternExample/creational/AbstractFactory/AbstractFactoryImpl1.java
+++ src/main/java/com/skyeYeh/designPatternExample/creational/AbstractFactory/AbstractFactoryImpl1.java
@@ -6,23 +6,6 @@ package com.skyeYeh.designPatternExample.creational.abstractFactory;
  */
 public class AbstractFactoryImpl1 implements AbstractFactory {
     /**
-     * 實作抽象工廠建立產品A。
-     *
-     * @param type 產品種類
-     * @return 產品A
-     */
-    @Override
-    public ProductA CreateProductA(String type) {
-        ProductA product = null;
-        if ("productAImpl1".equals(type)) {
-            product = new ProductAImpl1();
-        } else if ("productAImpl2".equals(type)) {
-            product = new ProductAImpl2();
-        }
-        return product;
-    }
-
-    /**
      * 實作抽象工廠建立產品B。
      *
      * @return 產品B
