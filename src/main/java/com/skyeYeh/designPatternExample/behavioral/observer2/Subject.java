775384135875b6d5429fd5cfae663fe63fdd866d
SkyeYeh <presto408@gmail.com>
2016-02-21 23:29:11 +0800

    Replace the type specification in this constructor call with the diamond operator ("<>").

--- src/main/java/com/skyeYeh/designPatternExample/behavioral/observer2/Subject.java
+++ src/main/java/com/skyeYeh/designPatternExample/behavioral/observer2/Subject.java
@@ -8,7 +8,7 @@ import java.util.List;
  * 主題物件。
  */
 public class Subject {
-    private List<Observer> observers = new ArrayList<Observer>();
+    private List<Observer> observers = new ArrayList<>();
     private int state;
 
     public void attach(Observer observer) {
